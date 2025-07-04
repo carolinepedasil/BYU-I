d2e3ba58e2103050f2c0893b9f6f0caae31bf5fb
Caroline Pereira da Silva <carolinepedasil@gmail.com>
2025-07-04 20:02:39 -0300

    WDD 330 - Web Frontend Development II

---
+++ WDD 330 - Web Frontend Development II/src/js/product.js
@@ -0,0 +1,15 @@
+import { getParam } from './utils.mjs';
+import ProductData from './ProductData.mjs';
+import ProductDetails from './ProductDetails.mjs';
+import { applyDiscountBadges } from './utils/price.js';
+import { renderBreadcrumbs } from './breadcrumbs.js';
+
+const productId = getParam('product');
+const dataSource = new ProductData('tents');
+const product = new ProductDetails(productId, dataSource);
+
+document.addEventListener('DOMContentLoaded', () => {
+  product.init();
+  applyDiscountBadges();
+  renderBreadcrumbs('Tents');
+});
